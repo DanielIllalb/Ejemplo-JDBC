bab89748c3f96f4752c4096ba81636eabbb36de5
DanielIllalb <daniel.illana.3004@fernando3martos.com>
2025-05-23 14:29:00 +0200

    Se añade try-catch for resources

--- src/main/java/Paso1.java
+++ src/main/java/Paso1.java
@@ -1,31 +1,34 @@
 import java.sql.*;
 
 public class Paso1 {
+
     public static void main(String[] args) throws SQLException {
 
         // Definición de los parámetros de conexión a la BBDD
 
         String url = "jdbc:postgresql://localhost:5432/jardineria";
-        String user = "postgres";
-        String pass = "root";
+        String user = "jardinero";
+        String pass = "jardinero";
 
-        try{
-            Connection con = DriverManager.getConnection(url, user, pass);
-            Statement stmt = con.createStatement();
+        // Se crean los recursos necesarios para la consulta fuera del try-catch
 
-            ResultSet rs = stmt.executeQuery("SELECT * FROM cliente");
-
+        try(
+                Connection con = DriverManager.getConnection(url, user, pass);
+                Statement stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery(
+                        "SELECT cl.nombre_cliente,em.nombre " +
+                        "FROM cliente cl " +
+                        "JOIN empleado em " +
+                        "ON cl.codigo_empleado_rep_ventas = em.codigo_empleado"
+                );
+        )
+        {
             while(rs.next()){
                 System.out.println(
-                        rs.getInt(1) + " "
-                        + rs.getString(2) + " "
-                        + rs.getString(3)
+                        rs.getString("nombre_cliente") + " "
+                        + rs.getString("nombre")
                 );
             }
-
-            rs.close();
-            stmt.close();
-            con.close();
 
         }catch (SQLException e) {
             throw new SQLException(e.getMessage());
